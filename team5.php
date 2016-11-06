e00279ad89a3a85f63504634e37a4b38c63b6233
cyang <cyang001@citymail.cuny.edu>
2016-11-06 12:14:55 -0500

    Replace semi colons

--- team5.php
+++ team5.php
@@ -19,7 +19,7 @@ $r_1 = $db->query($q_1);
 if ($r_1->num_rows > 0) {
     // output data of each row
     while($row = $r_1->fetch_assoc()) {
-        echo "stor_id: " . $row["stor_id"]. " | ord_num: " . $row["ord_num"]. " | title_id: " . $row["title_id"]. " | qty; " . $row["qty"].  " | discount; " . $row["discount"].  "<br>";
+        echo "stor_id: " . $row["stor_id"]. " | ord_num: " . $row["ord_num"]. " | title_id: " . $row["title_id"]. " | qty: " . $row["qty"].  " | discount: " . $row["discount"].  "<br>";
     }
 } else {
     echo "0 results";
