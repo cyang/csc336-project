a8879dec2c6b27741e3472ebee8c6388e322b17c
yang8294 <cyang001@citymail.cuny.edu>
2016-11-06 11:56:31 -0500

    Add team5.php

---
+++ team5.php
@@ -0,0 +1,29 @@
+<?php
+
+$host = "134.74.126.107";
+$username = "USERNAME";
+$password = "PASSWORD";
+$team_database = "F16336team5";
+// Connection
+$db = new mysqli($host, $username, $password, $team_database);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+} 
+
+$q_1 = "SELECT salesdetail.* FROM salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id";
+echo $q_1 . "<br>";
+$r_1 = $db->query($q_1);
+
+
+if ($r_1->num_rows > 0) {
+    // output data of each row
+    while($row = $r_1->fetch_assoc()) {
+        echo "stor_id: " . $row["stor_id"]. " | ord_num: " . $row["ord_num"]. " | title_id: " . $row["title_id"]. " | qty: " . $row["qty"].  " | discount: " . $row["discount"].  "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$db->close();
+?>
