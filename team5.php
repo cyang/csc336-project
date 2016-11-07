56afb908ca4e996f7740d68fa46e6196fe51890e
cyang <cyang001@citymail.cuny.edu>
2016-11-07 10:29:46 -0500

    Add config file

--- team5.php
+++ team5.php
@@ -1,15 +1,5 @@
 <?php
-
-$host = "134.74.126.107";
-$username = "USERNAME";
-$password = "PASSWORD";
-$team_database = "F16336team5";
-// Connection
-$db = new mysqli($host, $username, $password, $team_database);
-
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-} 
+require "config.php";
 
 $q_1 = "SELECT salesdetail.* FROM salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id";
 echo $q_1 . "<br>";
