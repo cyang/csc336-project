113007381a218486a34c98964116f5e91e29fbd7
cyang <cyang001@citymail.cuny.edu>
2016-11-07 10:14:20 -0500

    Add title_id to

--- team5.php
+++ team5.php
@@ -1,9 +1,8 @@
-#!/usr/bin/php
 <?php
 
 $host = "134.74.126.107";
-$username = "USERNAME";
-$password = "PASSWORD";
+$username = "F16336cyang";
+$password = "23158294";
 $team_database = "F16336team5";
 // Connection
 $db = new mysqli($host, $username, $password, $team_database);
@@ -25,8 +24,8 @@ if ($r_1->num_rows > 0) {
 } else {
     echo "0 results";
 }
-
-$q_2 = "SELECT stor_id, ord_num, qty*titles.price*discount/100 AS total from salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id;";
+echo "<br>";
+$q_2 = "SELECT stor_id, ord_num, titles.title_id, qty*titles.price*discount/100 AS total FROM salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id";
 echo $q_2 . "<br>";
 $r_2 = $db->query($q_2);
 if ($r_2->num_rows > 0) {
@@ -37,7 +36,7 @@ if ($r_2->num_rows > 0) {
 } else {
     echo "0 results";
 }
-
+echo "<br>";
 $q_3 = "SELECT SUM(qty*titles.price*discount/100) as gross_revenue from salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id;";
 echo $q_3 . "<br>";
 $r_3 = $db->query($q_3);
@@ -49,14 +48,14 @@ if ($r_3->num_rows > 0) {
 } else {
     echo "0 results";
 }
-
+echo "<br>";
 $q_4 = "SELECT stor_id, ord_num, SUM(qty*titles.price*discount/100) as gross_revenue FROM salesdetail INNER JOIN titles ON salesdetail.title_id=titles.title_id GROUP BY ord_num;";
 echo $q_4 . "<br>";
 $r_4 = $db->query($q_4);
 if ($r_4->num_rows > 0) {
     // output data of each row
     while($row = $r_4->fetch_assoc()) {
-        echo "stor_id: " . $row["stor_id"]. " | ord_num: " . $row["ord_num"]. "gross_revenue: " . $row["gross_revenue"]. "<br>";
+        echo "stor_id: " . $row["stor_id"]. " | ord_num: " . $row["ord_num"]. " | gross_revenue: " . $row["gross_revenue"]. "<br>";
     }
 } else {
     echo "0 results";
